232f2163bc1a28731d704cd1fad1f467455aa9a7
Enigma <jonpking@gmail.com>
2019-10-06 21:25:59 -0700

    created sectioned ItemCard component and added basic styling

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button.js";
+import "../ItemCard/ItemCard.css";
 
 function ItemCard(props) {
     return (
-        
+        <div className="itemCard">
+            <div className="itemCardImage">
+                <img src="https://via.placeholder.com/1000" alt="placeholder" />
+            </div>
+            <div className="itemCardInfoGroup">
+                <p className="itemName">This is an item name</p>
+                <p className="itemDetailsPreview">These are items details</p>
+                <div className="buttonGroup">
+                    <button type="">button 1</button>
+                    <button type="">button 2</button>
+                    <button type="">button 3</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
