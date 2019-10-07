3bd8e578bd714214eaaff12673ed598811d8858e
Enigma <jonpking@gmail.com>
2019-10-06 21:24:24 -0700

    added ItemCard component to Dashboard page

--- client/src/components/Pages/Dashboard.js
+++ client/src/components/Pages/Dashboard.js
@@ -5,6 +5,7 @@ import { logoutUser } from "../../actions/authActions";
 import Navbar from '../Navbar/Navbar'
 import { Link } from "react-router-dom";
 import Footer from "../Footer/Footer.js";
+import ItemCard from "../ItemCard/ItemCard";
 
 
 class Dashboard extends Component {
@@ -19,32 +20,36 @@ class Dashboard extends Component {
     return (
       <>
 
-      <Navbar />
+        <Navbar />
 
-      <Link to="/itemdetails">
-        Go to Item Details
+        <div id="itemCardContainer">
+          <ItemCard />
+        </div>
+
+        <Link to="/itemdetails">
+          Go to Item Details
       </Link>
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>Main User Page</span>
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="landing-copy col s12 center-align">
+              <h4>
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into a full-stack{" "}
+                  <span style={{ fontFamily: "monospace" }}>Main User Page</span>
+                </p>
+              </h4>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Logout
+  
             </button>
             </div>
           </div>
