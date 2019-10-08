7c6d297800fbca30890bc4aebf2692ebb63650d7
Enigma <jonpking@gmail.com>
2019-10-07 17:45:45 -0700

    added css file for Footer component and updated some Footer text

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
             <div className="container">
                 <div className="row">
                     <div className="col l6 s12">
-                        <h5 className="white-text">Footer Content</h5>
+                        <h5 className="white-text">RETRO-TRADE</h5>
                         <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
                     </div>
                     <div className="col l4 offset-l2 s12">
@@ -23,7 +23,7 @@ function Footer(props) {
             </div>
             <div className="footer-copyright">
                 <div className="container">
-                    © 2014 Copyright Text
+                    © 2019 RETRO-TRADE
             <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
                 </div>
             </div>
