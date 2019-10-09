8ceeceff0554f7ca8f1f6616ee9d7507a0c0ef86
Nathan <n8benzor@gmail.com>
2019-10-08 20:40:01 -0700

    fixed material-ui

--- client/src/components/Pages/Dashboard.js
+++ client/src/components/Pages/Dashboard.js
@@ -31,9 +31,7 @@ class Dashboard extends Component {
             itemDescription="This is a bunch of placeholder text that can be removed. It was only added to make the demo ItemCard component display properly. I am writing extra text in here so that it will properly test the overflow css." />
         </div>
 
-        <Link to="/itemdetails">
-          Go to Item Details
-      </Link>
+        
         <div style={{ height: "75vh" }} className="container valign-wrapper">
           <div className="row">
             <div className="landing-copy col s12 center-align">
