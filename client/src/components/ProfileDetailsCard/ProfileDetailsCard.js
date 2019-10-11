e05e027e75be74984048aeb2b90ea0cb01ce0944
Nathan <n8benzor@gmail.com>
2019-10-11 13:20:48 -0700

    styling updates to item details component

--- client/src/components/ProfileDetailsCard/ProfileDetailsCard.js
+++ client/src/components/ProfileDetailsCard/ProfileDetailsCard.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Button from "../Button/Button.js";
 import ProfileImage from "../ProfileImage/ProfileImage.js";
+import '../ProfileDetailsCard/ProfileDetailsCard.css';
 
 const ProfileDetailsCard = (props) => {
     return (
         <div className="col s12">
-           <ProfileImage />
         <h5 className="header center">Account Details</h5>
         <div className="card horizontal">
           <div className="card-image">
+           <ProfileImage />
           </div>
           <div className="card-stacked">
-            <div className="card-content">
+            <div className="card-content user-details">
               <ul>
                 <li><b>Name: </b>{props.first} {props.last}</li>
                 <li><b>Email: </b>{props.email}</li>
@@ -22,7 +22,7 @@ const ProfileDetailsCard = (props) => {
                 <li><b>Phone: </b>{props.phone}</li>
               </ul>
             </div>
-            <div className="card-action">
+            <div className="card-action right-align">
               <Button label="Edit" icon="edit"  />
             </div>
           </div>
