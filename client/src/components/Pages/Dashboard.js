b7724427750914816f9272d9aabcd42a125024e5
Enigma <jonpking@gmail.com>
2019-10-14 21:00:28 -0700

    updated color of logout button from blue to orange

--- client/src/components/Pages/Dashboard.js
+++ client/src/components/Pages/Dashboard.js
@@ -74,7 +74,7 @@ class Dashboard extends Component {
                   marginTop: "1rem"
                 }}
                 onClick={this.onLogoutClick}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                className="btn btn-large waves-effect waves-light hoverable orange darken-1"
               >
                 Logout
 
