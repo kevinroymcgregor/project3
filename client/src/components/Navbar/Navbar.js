9467d3a62f5f7a14ca43add3ce1e5a537029a5ea
Jonathan King <jonpking@gmail.com>
2019-10-23 12:36:29 -0700

    removed old commented code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -43,46 +43,41 @@ class Navbar extends Component {
   }
 
   render() {
-    const {  user } = this.props.auth;
+    const { user } = this.props.auth;
     // console.log(user)
 
-    const {avatar} = this.state.user;
+    const { avatar } = this.state.user;
     // console.log(avatar)
     const avatarLink = (
-      <>{ avatar ? `{avatar : ''}` : ''}</>
+      <>{avatar ? `{avatar : ''}` : ''}</>
     )
 
-    // const authLinks =(
-    //   <p>{ user ? `Welcome ${user.email }` : ''}</p>
-    // )
-    // console.log(this.state.user)
     return (
       <>
-      <div className="navbar-fixed">
+        <div className="navbar-fixed">
 
-        {/* Right side menu dropdown */}
-        <ul id="dropdown1" className="dropdown-content">
-          <li>
-            <Link to="/profile" userId={user.id}>Account</Link></li>
-          {/* <li><a href="#!">Messages</a></li> */}
-          <li className="divider"></li>
-          <li><a href="#!" onClick={this.onLogoutClick}>Logout</a></li>
-        </ul>
+          {/* Right side menu dropdown */}
+          <ul id="dropdown1" className="dropdown-content">
+            <li>
+              <Link to="/profile" userId={user.id}>Account</Link></li>
+            <li className="divider"></li>
+            <li><a href="#!" onClick={this.onLogoutClick}>Logout</a></li>
+          </ul>
 
-        {/* Main nav */}
-        <nav>
-          <div className="nav-wrapper">
-            <ul className="left">
+          {/* Main nav */}
+          <nav>
+            <div className="nav-wrapper">
+              <ul className="left">
                 <Link to="/dashboard" className="brand-logo">
-              <li>
-                  <p>RETRO-TRADE</p>
-              </li>
+                  <li>
+                    <p>RETRO-TRADE</p>
+                  </li>
                 </Link>
-              <li>
-                <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-              </li>
-            </ul>
-            
+                <li>
+                  <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                </li>
+              </ul>
+
               <div className="nav-wrapper search-form">
                 <form action="/dashboard" method="GET" className="hide-on-med-and-down">
                   <div className="input-field main">
@@ -93,53 +88,51 @@ class Navbar extends Component {
                 </form>
               </div>
 
-            <ul className="right">
-              <li>
-                <Link to="/addedititem" className="rightIcons"><i className="large material-icons nav-icons">add_circle</i></Link>
-              </li>
-              <li><Link to="/shopping-cart"><i className="material-icons">shopping_cart</i></Link></li>
-              <li className="hide-on-med-and-down"><a className="dropdown-trigger" href="#!" data-target="dropdown1"><i className="large material-icons nav-icons">person</i></a></li>
-            </ul>
+              <ul className="right">
+                <li>
+                  <Link to="/addedititem" className="rightIcons"><i className="large material-icons nav-icons">add_circle</i></Link>
+                </li>
+                <li><Link to="/shopping-cart"><i className="material-icons">shopping_cart</i></Link></li>
+                <li className="hide-on-med-and-down"><a className="dropdown-trigger" href="#!" data-target="dropdown1"><i className="large material-icons nav-icons">person</i></a></li>
+              </ul>
 
-          </div>
-        </nav>
-      </div>
-
-      {/* Sidenav */}
-      <ul id="slide-out" className="sidenav">
-        <li><div className="user-view">
-        <div className="background">
-          {/* <img src="images/office.jpg" /> */}
+            </div>
+          </nav>
         </div>
-        <img className="circle" src={avatar ? avatar : 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png'} />
-        <span className="white-text name">{this.state.user.firstName} {this.state.user.lastName}</span>
-        <span className="white-text email">{this.state.user.email}</span>
-        </div>
-        </li>     
-        <li><Link to="/dashboard"><i className="material-icons">home</i>Home</Link></li>
-        <li><Link to="/addedititem"><i className="material-icons">add_circle</i>Sell Item</Link></li>
-        <li><Link to="/shopping-cart"><i className="material-icons">shopping_cart</i>My Cart</Link></li>
-        <li><Link to="/profile"><i className="material-icons">settings</i>Account</Link></li>
-        <li><a href="#!" onClick={this.onLogoutClick}><i className="material-icons">person</i>Logout</a></li>
-        <li><div className="divider"></div></li>
 
-        <form action="/dashboard" method="GET">
-          <div className="input-field">
-            <input id="search" type="search" name="search" onKeyUp={this.handleSearch.bind(this)} required placeholder="Search..." />
-            <i className="material-icons">close</i>
+        {/* Sidenav */}
+        <ul id="slide-out" className="sidenav">
+          <li><div className="user-view">
+            <div className="background">
+            </div>
+            <img className="circle" src={avatar ? avatar : 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png'} />
+            <span className="white-text name">{this.state.user.firstName} {this.state.user.lastName}</span>
+            <span className="white-text email">{this.state.user.email}</span>
           </div>
-        </form>
-        
-        <li><a className="subheader">Categories</a></li>
-        <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Video Games</a></li>
-        <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Game Consoles</a></li>
-        <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Game Accessories</a></li>
-        <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Board Games</a></li>
-        <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Arcade</a></li>
+          </li>
+          <li><Link to="/dashboard"><i className="material-icons">home</i>Home</Link></li>
+          <li><Link to="/addedititem"><i className="material-icons">add_circle</i>Sell Item</Link></li>
+          <li><Link to="/shopping-cart"><i className="material-icons">shopping_cart</i>My Cart</Link></li>
+          <li><Link to="/profile"><i className="material-icons">settings</i>Account</Link></li>
+          <li><a href="#!" onClick={this.onLogoutClick}><i className="material-icons">person</i>Logout</a></li>
+          <li><div className="divider"></div></li>
 
-      </ul>
-  {/* <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons white">menu</i></a> */}
-        </>
+          <form action="/dashboard" method="GET">
+            <div className="input-field">
+              <input id="search" type="search" name="search" onKeyUp={this.handleSearch.bind(this)} required placeholder="Search..." />
+              <i className="material-icons">close</i>
+            </div>
+          </form>
+
+          <li><a className="subheader">Categories</a></li>
+          <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Video Games</a></li>
+          <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Game Consoles</a></li>
+          <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Game Accessories</a></li>
+          <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Board Games</a></li>
+          <li><a className="waves-effect" onClick={e => this.props.callbackFromParent(e.target.text)}>Arcade</a></li>
+
+        </ul>
+      </>
     );
   }
 }
@@ -156,6 +149,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { logoutUser }
-   )(Navbar);
-
-
+)(Navbar);
